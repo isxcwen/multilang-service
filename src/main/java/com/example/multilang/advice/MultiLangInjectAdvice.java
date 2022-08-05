897bf5ec85ffb4e65b8085c86113b36cda33a770
xc <isxc.wen@qq.com>
2022-08-05 18:47:06 +0800

    Initial commit

--- src/main/java/com/example/multilang/advice/MultiLangInjectAdvice.java
+++ src/main/java/com/example/multilang/advice/MultiLangInjectAdvice.java
@@ -15,10 +15,8 @@ import org.springframework.util.ReflectionUtils;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class MultiLangInjectAdvice implements ResponseBodyAdvice {
@@ -36,30 +34,15 @@ public class MultiLangInjectAdvice implements ResponseBodyAdvice {
         List<BaseMultiLang> data = getData(body);
         if(data != null && !data.isEmpty()){
             Class<? extends BaseMultiLang> aClass = data.get(0).getClass();
+
             MultiLangConfiguration.MultiLangCache multiLangCache = MultiLangConfiguration.getMultiColumn(aClass);
+
             String tableName = MultiLangUtils.getTableName(aClass);
             List<MultiLangModel> multiLangByTableIds = multiLangService.getMultiLangByTableIds(tableName, data.stream().map(BaseMultiLang::getId).collect(Collectors.toList()));
-            Map<String, Field> contentFieldMap = multiLangCache.getContentFieldMap();
-            data.stream().forEach(baseMultiLang -> {
-                //baseMultiLang.
-            });
 
-            Map<String, List<Long>> table2id = new HashMap<>();
-            data.stream().forEach(baseMultiLang -> {
-                String mainTable = aClass.getAnnotation(TableName.class).value();
-                List<Long> ids = getIds(table2id, mainTable);
-                ids.add(baseMultiLang.getId());
-            });
-            Object obj = null;
-            ReflectionUtils.doWithFields(aClass, field -> {
-                if(field.getDeclaringClass().isAnnotationPresent(TableName.class)){
-                    BaseMultiLang baseMultiLang = (BaseMultiLang)field.get(obj);
-                    String mainTable = aClass.getAnnotation(TableName.class).value();
-                    List<Long> ids = getIds(table2id, mainTable);
-                    ids.add(baseMultiLang.getId());
-                }
-            });
+            multiLangHandle(data, multiLangByTableIds, multiLangCache);
 
+            Collection<MultiLangConfiguration.MultiLangCache> nested = multiLangCache.getNested();
         }
         return null;
     }
@@ -79,6 +62,34 @@ public class MultiLangInjectAdvice implements ResponseBodyAdvice {
 
     public BaseMultiLang getSigleData(Object body){
         return null;
+    }
+
+    public void multiLangHandle(List<BaseMultiLang> baseMultiLangs, List<MultiLangModel> multiLangByTableIds, MultiLangConfiguration.MultiLangCache multiLangCache) {
+        if(baseMultiLangs == null || baseMultiLangs.isEmpty()){
+            return;
+        }
+        Map<Long, MultiLangModel> collect = multiLangByTableIds.stream().collect(Collectors.toMap(MultiLangModel::getId, Function.identity()));
+        if(collect == null || collect.isEmpty()){
+            return;
+        }
+        Map<String, Field> contentFieldMap = multiLangCache.getContentFieldMap();
+
+        if(contentFieldMap == null || contentFieldMap.isEmpty()){
+            return;
+        }
+
+        baseMultiLangs.stream().forEach(baseMultiLang -> {
+            Long id = baseMultiLang.getId();
+            MultiLangModel multiLangModel = collect.get(id);
+            if(multiLangCache != null){
+                Field field = contentFieldMap.get(multiLangModel.getColumnName());
+                try {
+                    field.set(baseMultiLang, multiLangModel.getContents());
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 
 }
